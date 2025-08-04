53fffb19f3c63cd1692135cb18c2ef881da5edc5
Jxavier <182994886+JxavierP@users.noreply.github.com>
2025-08-04 06:47:07 -0400

    init: Initial project creation and setup

---
+++ src/routes/index.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main class="text-gray-800 flex flex-col h-screen items-center justify-center">
+      <div class="text-center border-2 rounded-sm py-6 px-10">Welcome</div>
+    </main>
+  );
+}
