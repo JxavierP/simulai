357ca30f403ec5b5effba4f3648f20cd70621c89
Jxavier Paul <182994886+JxavierP@users.noreply.github.com>
2025-08-06 19:05:17 +0000

    style: wip sections

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,17 +2,31 @@ import Container from "~/components/Container";
 
 export default function Home() {
   return (
-    <main class="text-gray-800 flex h-screen">
-      <div class="w-3/12 text-center rounded-sm py-6 px-10">Lmfao</div>
-      <div class="flex flex-1 rounded-sm py-6 px-10">
+    <main class="text-gray-800 flex h-screen p-10 gap-10">
+      <div class="w-3/12 text-center rounded-sm">
+        <Container
+          class="w-full h-full bg-white border-violet-500"
+          decoratorColor="bg-violet-500"
+        >
+          <h1 class="font-semibold text-3xl">Control Panel</h1>
+        </Container>
+      </div>
+      <div class="flex flex-1 text-center rounded-sm">
         <Container
           class="w-full h-full bg-white border-blue-500"
           decoratorColor="bg-blue-500"
         >
-          Heya
+          <h1 class="font-semibold text-3xl">Social Feed</h1>
         </Container>
       </div>
-      <div class="w-2/12 text-center rounded-sm py-6 px-10">Welcome</div>
+      <div class="w-2/12 text-center rounded-sm">
+        <Container
+          class="w-full h-full bg-white border-black"
+          decoratorColor="bg-black"
+        >
+          <h1 class="font-semibold text-3xl">Logs</h1>
+        </Container>
+      </div>
     </main>
   );
 }
