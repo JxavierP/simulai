66dc74e67d801b8f260782c2cf471481a614dacc
Jxavier <182994886+JxavierP@users.noreply.github.com>
2025-08-10 08:18:41 -0400

    style: minor tweaks

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,13 +2,13 @@ import Container from "~/components/Container";
 
 export default function Home() {
   return (
-    <main class="text-gray-800 flex h-screen p-10 gap-10">
+    <main class="text-gray-800 flex h-screen p-10 gap-5">
       <div class="w-3/12 text-center rounded-sm">
         <Container
           class="w-full h-full bg-white border-violet-500"
           decoratorColor="bg-violet-500"
         >
-          <h1 class="font-semibold text-3xl">Control Panel</h1>
+          <h1 class="font-semibold text-2xl">S I M U L A I</h1>
         </Container>
       </div>
       <div class="flex flex-1 text-center rounded-sm">
@@ -16,15 +16,13 @@ export default function Home() {
           class="w-full h-full bg-white border-blue-500"
           decoratorColor="bg-blue-500"
         >
-          <h1 class="font-semibold text-3xl">Social Feed</h1>
         </Container>
       </div>
-      <div class="w-2/12 text-center rounded-sm">
+      <div class="w-3/12 text-center rounded-sm">
         <Container
           class="w-full h-full bg-white border-black"
           decoratorColor="bg-black"
         >
-          <h1 class="font-semibold text-3xl">Logs</h1>
         </Container>
       </div>
     </main>
